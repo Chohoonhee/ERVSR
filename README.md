5cce13582c7fb527d39401be42b25c4794af91ab
Chohoonhee <gnsgnsgml@kaist.ac.kr>
2022-07-28 20:45:02 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,243 @@
+## Reference-based Video Super-Resolution (RefVSR)<br><sub>Official PyTorch Implementation of the CVPR 2022 Paper</sub><br><sub>[Project](https://junyonglee.me/projects/RefVSR) | [Paper](https://openaccess.thecvf.com/content/CVPR2022/papers/Lee_Reference-Based_Video_Super-Resolution_Using_Multi-Camera_Video_Triplets_CVPR_2022_paper.pdf) | [arXiv](https://arxiv.org/abs/2203.14537) | [RealMCVSR Dataset](https://junyonglee.me/datasets/RealMCVSR)</sub><br><sub><sub>[![Hugging Face Spaces](https://img.shields.io/badge/%F0%9F%A4%97Hugging%20Face-Run%20in%20Spaces%20(CPU)-blue)](https://huggingface.co/spaces/codeslake/RefVSR) [![License CC BY-NC](https://img.shields.io/badge/Replicate-Run_in_Replicate_(GPU)-blue.svg?style=flat)](https://replicate.ai/codeslake/refvsr-cvpr2022)<br>[![PWC](https://img.shields.io/endpoint.svg?url=https://paperswithcode.com/badge/reference-based-video-super-resolution-using/reference-based-video-super-resolution-on)](https://paperswithcode.com/sota/reference-based-video-super-resolution-on?p=reference-based-video-super-resolution-using)</sub></sub>
+
+This repo contains training and evaluation code for the following paper:
+
+> [**Reference-based Video Super-Resolution Using Multi-Camera Video Triplets**](https://junyonglee.me/projects/RefVSR)<br>
+> [Junyong Lee](https://junyonglee.me), Myeonghee Lee, [Sunghyun Cho](https://www.scho.pe.kr/), and [Seungyong Lee](http://cg.postech.ac.kr/leesy/)<br>
+> POSTECH<br>
+> *IEEE Computer Vision and Pattern Recognition (**CVPR**) 2022*<br>
+
+
+<p align="left">
+<a href="https://junyonglee.me/projects/RefVSR">
+    <!--<img width=100% src="https://i.imgur.com/DryOd2I.gif"/>-->
+    <img width=100% src="https://i.imgur.com/H2ioB72.gif"/>
+</a><br>
+</p>
+
+## Getting Started
+### Prerequisites
+
+*Tested environment*
+
+![Ubuntu](https://img.shields.io/badge/Ubuntu-18.0.4-blue.svg?style=plastic)
+![Python](https://img.shields.io/badge/Python-3.8.8-green.svg?style=plastic)
+![PyTorch](https://img.shields.io/badge/PyTorch-1.8.0%20&%201.10.2%20&%201.11.0-green.svg?style=plastic)
+![CUDA](https://img.shields.io/badge/CUDA-10.2%20&%2011.1%20&%2011.3-green.svg?style=plastic)
+
+#### 1. Environment setup
+```bash
+$ git clone https://github.com/codeslake/RefVSR.git
+$ cd RefVSR
+
+$ conda create -y name RefVSR python 3.8 && conda activate RefVSR
+
+# Install pytorch
+$ conda install pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
+
+# Install requirements
+$ ./install/install_cudnn113.sh
+```
+
+> It is recommended to install PyTorch >= 1.10.0 with CUDA11.3 for running small models using Pytorch AMP, because PyTorch < 1.10.0 is known to have [a problem in running amp with `torch.nn.functional.grid_sample()`](https://github.com/pytorch/pytorch/issues/42218) needed for inter-frame alignment.
+
+> For the other models, PyTorch 1.8.0 is verified. To install requirements with PyTorch 1.8.0, run `./install/install_cudnn102.sh` for CUDA10.2 or `./install/install_cudnn111.sh` for CUDA11.1
+
+#### 2. Dataset
+Download and unzip the proposed [RealMCVSR dataset](https://junyonglee.me/datasets/RealMCVSR) under `[DATA_OFFSET]`:
+
+```
+[DATA_OFFSET]
+    └── RealMCVSR
+        ├── train                       # a training set
+        │   ├── HR                      # videos in original resolution 
+        │   │   ├── T                   # telephoto videos
+        │   │   │   ├── 0002            # a video clip 
+        │   │   │   │   ├── 0000.png    # a video frame
+        │   │   │   │   └── ...         
+        │   │   │   └── ...            
+        │   │   ├── UW                  # ultra-wide-angle videos
+        │   │   └── W                   # wide-angle videos
+        │   ├── LRx2                    # 2x downsampled videos
+        │   └── LRx4                    # 4x downsampled videos
+        ├── test                        # a testing set
+        └── valid                       # a validation set
+```
+
+> `[DATA_OFFSET]` can be modified with `--data_offset` option in the evaluation script.
+
+#### 3. Pre-trained models
+Download pretrained weights ([Google Drive](https://drive.google.com/drive/folders/1LtTYylYvOegybD4DbqqBhutn2_gfIBwZ?usp=sharing) | [Dropbox](https://www.dropbox.com/sh/pyirbf2jp0uxoq8/AAA6MEuSJYLcwQLVdx3s12Lea?dl=0)) under `./ckpt/`:
+
+```
+RefVSR
+├── ...
+├── ./ckpt
+│   ├── edvr.pytorch                    # weights of EDVR modules used for training Ours-IR
+│   ├── SPyNet.pytorch                  # weights of SpyNet used for inter-frame alignment
+│   ├── RefVSR_small_L1.pytorch         # weights of Ours-small-L1
+│   ├── RefVSR_small_MFID.pytorch       # weights of Ours-small
+│   ├── RefVSR_small_MFID_8K.pytorch    # weights of Ours-small-8K
+│   ├── RefVSR_L1.pytorch               # weights of Ours-L1
+│   ├── RefVSR_MFID.pytorch             # weights of Ours
+│   ├── RefVSR_MFID_8K.pytorch.pytorch  # weights of Ours-8K
+│   ├── RefVSR_IR_MFID.pytorch          # weights of Ours-IR
+│   └── RefVSR_IR_L1.pytorch            # weights of Ours-IR-L1
+└── ...
+```
+
+---
+***For the testing and training of your own model, it is recommended to go through wiki pages for<br>
+[logging](https://github.com/codeslake/RefVSR/wiki/Log-Details) and [details of testing and training scripts](https://github.com/codeslake/RefVSR/wiki/Details-of-Testing-&-Training-scripts) before running the scripts.***
+
+## Testing models of CVPR 2022
+#### Evaluation script
+```bash
+CUDA_VISIBLE_DEVICES=0 python -B run.py \
+    --mode _RefVSR_MFID_8K \                       # name of the model to evaluate
+    --config config_RefVSR_MFID_8K \               # name of the configuration file in ./configs
+    --data RealMCVSR \                             # name of the dataset
+    --ckpt_abs_name ckpt/RefVSR_MFID_8K.pytorch \  # absolute path for the checkpoint
+    --data_offset /data1/junyonglee \              # offset path for the dataset (e.g., [DATA_OFFSET]/RealMCVSR)
+    --output_offset ./result                       # offset path for the outputs
+```
+
+### Real-world 4x video super-resolution (HD to 8K resolution)
+```bash
+# Evaluating the model 'Ours' (Fig. 8 in the main paper).
+$ ./scripts_eval/eval_RefVSR_MFID_8K.sh
+
+# Evaluating the model 'Ours-small'.
+$ ./scripts_eval/eval_amp_RefVSR_small_MFID_8K.sh
+```
+> For the model `Ours`, we use Nvidia Quadro 8000 (48GB) in practice.
+
+> For the model `Ours-small`,
+> * We use Nvidia GeForce RTX 3090 (24GB) in practice.
+> * It is the model `Ours-small` in Table 2 further trained with the adaptation stage.
+> * The model requires PyTorch >= 1.10.0 with CUDA 11.3 for using PyTorch AMP.
+
+
+### Quantitative evaluation (models trained with the pre-training stage)
+```bash
+## Table 2 in the main paper
+# Ours
+$ ./scripts_eval/eval_RefVSR_MFID.sh
+
+# Ours-l1
+$ ./scripts_eval/eval_RefVSR_L1.sh
+
+# Ours-small
+$ ./scripts_eval/eval_amp_RefVSR_small_MFID.sh
+
+# Ours-small-l1
+$ ./scripts_eval/eval_amp_RefVSR_small_L1.sh
+
+# Ours-IR
+$ ./scripts_eval/eval_RefVSR_IR_MFID.sh
+
+# Ours-IR-l1
+$ ./scripts_eval/eval_RefVSR_IR_L1.sh
+```
+> For all models, we use Nvidia GeForce RTX 3090 (24GB) in practice.
+
+> To obtain quantitative results measured with the varying FoV ranges as shown in Table 3 of the main paper, modify the script and specify `--eval_mode FOV`.
+
+
+## Training models with the proposed two-stage training strategy
+
+### The pre-training stage (Sec. 4.1)
+
+```bash
+# To train the model 'Ours':
+$ ./scripts_train/train_RefVSR_MFID.sh
+
+# To train the model 'Ours-small':
+$ ./scripts_train/train_amp_RefVSR_small_MFID.sh
+```
+> For both models, we use Nvidia GeForce RTX 3090 (24GB) in practice.
+
+> Be sure to modify the script file and set proper GPU devices, number of GPUs, and batch size by modifying `CUDA_VISIBLE_DEVICES`, `--nproc_per_node` and `-b` options, respectively.
+> * We use the ***total batch size of 4***, the multiplication of numbers in options `--nproc_per_node` and `-b`.
+
+
+### The adaptation stage (Sec. 4.2)
+1. Set the path of the checkpoint of a model trained with the pre-training stage.<br>For the model `Ours-small`, for example,
+    ```bash
+    $ vim ./scripts_train/train_amp_RefVSR_small_MFID_8K.sh
+    ```
+    ```bash
+    #!/bin/bash
+
+    py3clean ./
+    CUDA_VISIBLE_DEVICES=0,1 ...
+        ...
+        -ra [LOG_OFFSET]/RefVSR_CVPR2022/amp_RefVSR_small_MFID/checkpoint/train/epoch/ckpt/amp_RefVSR_small_MFID_00xxx.pytorch
+        ...
+
+    ```
+    > Checkpoint path is `[LOG_OFFSET]/RefVSR_CVPR2022/[mode]/checkpoint/train/epoch/[mode]_00xxx.pytorch`.
+    > * PSNR is recorded in `[LOG_OFFSET]/RefVSR_CVPR2022/[mode]/checkpoint/train/epoch/checkpoint.txt`.
+    > * `[LOG_OFFSET]` can be modified with [`config.log_offset`](https://github.com/codeslake/RefVSR/blob/main/configs/config.py#L71) in `./configs/config.py`.
+    > * `[mode]` is the name of the model assigned with `--mode` in the script used for the pre-training stage.
+
+2. Start the adaptation stage.
+
+    ```shell
+    # Training the model 'Ours'.
+    $ ./scripts_train/train_RefVSR_MFID_8K.sh
+
+    # Training the model 'Ours-small'.
+    $ ./scripts_train/train_amp_RefVSR_small_MFID_8K.sh
+    ```
+
+    > For the model `Ours`, we use Nvidia Quadro 8000 (48GB) in practice.
+
+    > For the model `Ours-small`, we use Nvidia GeForce RTX 3090 (24GB) in practice.
+
+    > Be sure to modify the script file to set proper GPU devices, number of GPUs, and batch size by modifying `CUDA_VISIBLE_DEVICES`, `--nproc_per_node` and `-b` options, respectively.
+    > * We use the ***total batch size of 2***, the multiplication of numbers in options `--nproc_per_node` and `-b`.
+
+## Training models with L1 loss
+```bash
+# To train the model 'Ours-l1':
+$ ./scripts_train/train_RefVSR_L1.sh
+
+# To train the model 'Ours-small-l1':
+$ ./scripts_train/train_amp_RefVSR_small_L1.sh
+
+# To train the model 'Ours-IR-l1':
+$ ./scripts_train/train_amp_RefVSR_small_L1.sh
+```
+> For all models, we use Nvidia GeForce RTX 3090 (24GB) in practice.
+
+> Be sure to modify the script file and set proper GPU devices, number of GPUs, and batch size by modifying `CUDA_VISIBLE_DEVICES`, `--nproc_per_node` and `-b` options, respectively.
+> * We use the ***total batch size of 8***, the multiplication of numbers in options `--nproc_per_node` and `-b`.
+
+
+## Wiki
+* [Logging](https://github.com/codeslake/RefVSR/wiki/Log-Details)
+* [Details of testing and training scripts](https://github.com/codeslake/RefVSR/wiki/Details-of-Testing-&-Training-scripts)
+
+## Contact
+Open an issue for any inquiries.
+You may also have contact with [junyonglee@postech.ac.kr](mailto:junyonglee@postech.ac.kr)
+
+## License
+![License CC BY-NC](https://img.shields.io/badge/license-GNU_AGPv3-blue.svg?style=plastic)
+
+This software is being made available under the terms in the [LICENSE](LICENSE) file.
+Any exemptions to these terms require a license from the Pohang University of Science and Technology.
+
+## Acknowledgment
+We thank the authors of [BasicVSR](https://github.com/ckkelvinchan/BasicVSR-IconVSR) and [DCSR](https://github.com/Tengfei-Wang/DCSR) for sharing their code.
+
+#### BibTeX
+```
+@InProceedings{Lee2022RefVSR,
+    author    = {Junyong Lee and Myeonghee Lee and Sunghyun Cho and Seungyong Lee},
+    title     = {Reference-based Video Super-Resolution Using Multi-Camera Video Triplets},
+    booktitle = {Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+    year      = {2022}
+}
+```
---
+++ trainers/trainer.py
@@ -0,0 +1,268 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.parallel import DataParallel as DP
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+import os
+import collections
+from shutil import copy2
+import gc
+
+import numpy as np
+
+from utils import toGreen, toRed, print_err
+from data_loader.utils import refine_image_pt
+from trainers.utils import clone_detach_dict
+from trainers.baseTrainer import baseTrainer
+
+from models.SRNet import SRNet
+from models.loss.Loss import Loss
+from models.utils import warp
+
+#import fvcore.nn as fvnn
+from ptflops import get_model_complexity_info
+
+class Trainer(baseTrainer):
+    def __init__(self, config):
+        super(Trainer, self).__init__(config)
+
+        self.device = config.device
+        self.rank = torch.distributed.get_rank() if config.dist else -1
+        self.ws = torch.distributed.get_world_size() if config.dist else 1
+        self.itr_global = {'train': 0, 'valid': 0}
+        self.itr_inc = {'train': self.config.frame_itr_num, 'valid': 1}
+
+        ### NETWORKS ###
+        if self.rank <= 0 : print(toGreen('Loading Model...'))
+        self.network = SRNet(config).to(self.device, non_blocking=True)
+
+
+        if self.is_train and self.config.resume is None or self.is_train and os.path.exists('./models/archs/{}.py'.format(config.network)):
+            copy2('./models/archs/{}.py'.format(config.network), self.config.LOG_DIR.offset)
+            copy2('./trainers/{}.py'.format(config.trainer), self.config.LOG_DIR.offset)
+
+        ### INIT for training ###
+        if self.is_train:
+            if self.config.is_amp:
+                self.scaler = torch.cuda.amp.GradScaler()
+            self.network.init()
+            self._set_dataloader()
+            self._set_optim()
+            self._set_lr_scheduler()
+            self.Loss = Loss(config)
+            self.l1_loss = nn.L1Loss()
+            if config.is_verbose:
+                for name, param in self.network.named_parameters():
+                    if self.rank <= 0: print(name, ', ', param.requires_grad)
+        else:
+            if config.EVAL.is_replicate is False:
+                self._set_dataloader(self.is_train)
+
+        ### DDP ###
+        if config.cuda and (config.EVAL.is_replicate is False or config.EVAL.is_gradio is False):
+            if config.dist:
+                if self.rank <= 0: print(toGreen('Building Dist Parallel Model...'))
+                self.network = DDP(self.network, device_ids=[torch.cuda.current_device()], output_device=torch.cuda.current_device(), broadcast_buffers=True, find_unused_parameters=False)
+            else:
+                self.network = DP(self.network).to(self.device, non_blocking=True)
+
+            # if self.config.is_train and self.rank <= 0:
+            # ### PROFILE ###
+            # if self.rank <= 0:
+            #     #with torch.no_grad():
+            #     #     # input_size = (1, self.config.frame_num, 3, 1080//4, 1920//4)
+            #     #     input_size = (1, self.config.frame_num, 3, 256, 256)
+            #     #     if config.dist:
+            #     #         inputs = self.network.module.input_constructor(input_size)
+            #     #     else:
+            #     #         try:
+            #     #             inputs = self.network.input_constructor(input_size)
+            #     #         except:
+            #     #             inputs = self.network.module.input_constructor(input_size)
+            #     #     inputs = tuple(inputs.values())
+            #     #     Macs = fvnn.FlopCountAnalysis(self.network, inputs=inputs).total()
+            #     #     params = fvnn.parameter_count(self.network)['']
+            #     with torch.no_grad():
+            #         if config.flag_HD_in is False:
+            #             res = (1, config.frame_num, 3, 1080//config.scale, 1090//config.scale)
+            #         else:
+            #             res = (1, config.frame_num, 3, 1080, 1920)
+            #         Macs,params = get_model_complexity_info(self.network, res, input_constructor = self.network.module.input_constructor, as_strings=False, print_per_layer_stat=config.is_verbose)
+
+            # if self.rank <= 0:
+            #     print(toGreen('Computing model complexity...'))
+            #     print('{:<30}  {:<8} B'.format('Computational complexity (Macs): ', Macs / 1000 ** 3 ))
+            #     print('{:<30}  {:<8} M'.format('Number of parameters: ',params / 1000 ** 2))
+            #     if self.is_train:
+            #         with open(config.LOG_DIR.offset + '/cost.txt', 'w') as f:
+            #             f.write('{:<30}  {:<8} B\n'.format('Computational complexity (Macs): ', Macs / 1000 ** 3 ))
+            #             f.write('{:<30}  {:<8} M'.format('Number of parameters: ',params / 1000 ** 2))
+            #             f.close()
+
+            # gc.collect()
+            # torch.cuda.empty_cache()
+
+    ######################################################################################################
+    ########################### Edit from here for training/testing scheme ###############################
+    ######################################################################################################
+
+    def _set_results(self, inputs, outs, errs, log, norm_=1, is_train=False):
+        ## save visuals (inputs)
+        self.results['vis'] = collections.OrderedDict()
+        self.results['vis']['LR_UW{}'.format('' if not self.config.flag_HD_in else '')] = outs['LR_UW']
+        self.results['vis']['SR_UW{}'.format('' if not self.config.flag_HD_in else '')] = outs['result'].detach()
+        self.results['vis']['HR_UW'] = outs['HR_UW']
+        self.results['vis']['LR_REF_W'] = outs['LR_REF_W']
+        if is_train:
+            self.results['vis']['HR_REF_W'] = outs['HR_REF_W']
+
+        if self.config.save_sample:
+            ## save visuals (outputs)
+            if 'vis' in outs.keys():
+                self.results['vis'].update(outs['vis'].items())
+
+        # rotate roated portrait frames (which have been rotated 90 in data loader) to original rotation (due to memory issue during valudation in training)
+        # if 'is_portrait' in inputs.keys() and inputs['is_portrait']:
+        #     for k, v in self.results['vis'].items():
+        #         self.results['vis'][k] = torch.rot90(v, 3, [v.dim()-2, v.dim()-1])
+        # print('\n\n', self.rank, inputs['is_portrait'], inputs['LR_UW'].size(), inputs['LR_UW'].is_contiguous(), self.results['vis']['SR_UW'].size(), '\n\n')
+
+        ## Essentials ##
+        # save scalars
+        self.results['errs'] = errs
+        self.results['norm'] = norm_
+        # learning rate
+        self.results['log_etc'] = log
+
+    def iteration(self, inputs, is_log, is_train):
+        ## Init for logging
+        state = 'train' if is_train else 'valid'
+        self.itr_global[state] += self.itr_inc[state]
+
+        ## Prepare data
+        LR_UW_total_frames = refine_image_pt(inputs['LR_UW'].to(self.device, non_blocking=True), self.config.refine_val_lr)
+        LR_REF_W_total_frames = refine_image_pt(inputs['LR_REF_W'].to(self.device, non_blocking=True), self.config.refine_val_lr)
+        # LR_REF_W_frame = LR_REF_W_total_frames[:, LR_REF_W_total_frames.size(1)//2, :, :, :].to(self.device, non_blocking=True)
+        HR_UW_total_frames = refine_image_pt(inputs['HR_UW'].to(self.device, non_blocking=True), self.config.refine_val_hr)
+
+        if self.config.is_use_T:
+            LR_REF_T_total_frames = refine_image_pt(inputs['LR_REF_T'].to(self.device, non_blocking=True), self.config.refine_val_lr) if self.config.is_use_T else None
+            HR_REF_T_total_frames = refine_image_pt(inputs['HR_REF_T'].to(self.device, non_blocking=True), self.config.refine_val_hr) if self.config.is_use_T else None
+
+        HR_REF_W_total_frames = refine_image_pt(inputs['HR_REF_W'].to(self.device, non_blocking=True), self.config.refine_val_lr)
+
+        norm_ = 0
+        b, total_frame_num, c, h, w = LR_UW_total_frames.size()
+        errs_total = collections.OrderedDict()
+
+        ## Iteration
+        for i in range(total_frame_num-(self.config.frame_num - 1)):
+            is_first_frame = i == 0 if is_train else inputs['is_first'][0].item()
+
+            LR_UW_frames = LR_UW_total_frames[:, i:i+self.config.frame_num]
+            LR_REF_W_frames = LR_REF_W_total_frames[:, i:i+self.config.frame_num]
+            LR_REF_W_frame = LR_REF_W_frames[:, self.config.frame_num//2, :, :, :].to(self.device, non_blocking=True)
+            HR_UW_frames = HR_UW_total_frames[:, i:i+self.config.frame_num]
+            HR_UW = HR_UW_frames[:, self.config.frame_num//2]
+
+            #################################################################################################
+
+            if self.config.is_amp:
+                with torch.cuda.amp.autocast():
+                    outs = self.network(LR_UW_frames, LR_REF_W_frame, is_first_frame, is_log, is_train)
+            else:
+                outs = self.network(LR_UW_frames, LR_REF_W_frame, is_first_frame, is_log, is_train)
+
+            #################################################################################################
+
+            if self.config.is_use_T:
+                REF_frames = HR_REF_T_total_frames[:, :i+self.config.frame_num]
+            else:
+                REF_frames = HR_REF_W_total_frames[:, :i+self.config.frame_num]
+
+            # errs = self.Loss.get_loss(outs['result'], HR_UW, REF_frames, is_train, is_log, outs)            
+            errs = self.Loss.get_loss(outs['result'], HR_UW_frames, REF_frames, is_train, is_log, outs)
+            # errs = self.l1_loss(outs, HR_UW_frames)
+
+
+            ## Updating network & get log (learning rate, gnorm)
+            # if self.config.is_amp:
+            #     log = self._update_amp(errs) if is_train else None
+            # else:
+            log = self._update(errs) if is_train else None
+            
+            
+
+            # Loggging
+            with torch.no_grad():
+                norm_ += b
+                if errs is not None:
+                    for k, v in errs.items():
+                        v_t = 0 if i == 0 else errs_total[k]
+                        # errs_total[k] = v_t + v.item() if isinstance(v, torch.Tensor) else v * b
+                        errs_total[k] = v_t + b * v.detach().clone() if isinstance(v, torch.Tensor) else v
+
+        # assert norm_ != 0
+        ## setting results for the log
+        if is_log:
+            outs['LR_UW'] = LR_UW_frames[:, self.config.frame_num//2]
+            outs['HR_UW'] = HR_UW_frames[:, self.config.frame_num//2]
+            outs['LR_REF_W'] = LR_REF_W_frames[:, self.config.frame_num//2]
+            if is_train:
+                HR_REF_W_frames = HR_REF_W_total_frames[:, i:i+self.config.frame_num]
+                outs['HR_REF_W'] = HR_REF_W_frames[:, self.config.frame_num//2]
+            if self.config.is_use_T and 'vis' in outs.keys():
+                LR_REF_T_frames = LR_REF_T_total_frames[:, i:i+self.config.frame_num]
+                outs['vis']['LR_REF_T'] = LR_REF_T_frames[:, self.config.frame_num//2]
+                if is_train:
+                    HR_REF_T_frames = HR_REF_T_total_frames[:, i:i+self.config.frame_num]
+                    outs['vis']['HR_REF_T'] = HR_REF_T_frames[:, self.config.frame_num//2]
+
+            if 'vis' not in outs.keys():
+                outs['vis'] = collections.OrderedDict()
+            else:
+                pass
+
+            self._set_results(inputs, clone_detach_dict(outs), errs_total, log, norm_, is_train)
+
+    def evaluation(self, inputs, is_log=False, is_PSNR=False):
+        # prepare data
+        LR_UW_total_frames = refine_image_pt(inputs['LR_UW'].to(self.device, non_blocking=True), self.config.refine_val_lr).contiguous()
+        LR_REF_W_total_frames = refine_image_pt(inputs['LR_REF_W'].to(self.device, non_blocking=True), self.config.refine_val_lr).contiguous()
+
+        b, total_frame_num, c, h, w = LR_UW_total_frames.size()
+        errs_total = collections.OrderedDict()
+
+        is_first_frame = inputs['is_first'][0].item()
+
+        LR_UW_frames = LR_UW_total_frames[:, :self.config.frame_num]
+        LR_REF_W_frames = LR_REF_W_total_frames[:, :self.config.frame_num]
+
+        #################################################################################################
+
+        if self.config.is_amp:
+            with torch.cuda.amp.autocast():
+                outs = self.network(LR_UW_frames, LR_REF_W_frames, is_first_frame, is_log=is_log, is_train=False)
+        else:
+            outs = self.network(LR_UW_frames, LR_REF_W_frames, is_first_frame, is_log=is_log, is_train=False)
+
+        #################################################################################################
+
+        ## setting results for the log
+        result = collections.OrderedDict()
+        result['vis'] = collections.OrderedDict()
+        result['vis']['LR_UW'] = LR_UW_frames[:, self.config.frame_num//2]
+        result['vis']['SR_UW'] = outs['result'][:, self.config.frame_num//2]
+        result['vis']['HR_UW'] = refine_image_pt(inputs['HR_UW'][:, self.config.frame_num//2].to(self.device, non_blocking=True), self.config.refine_val_hr) 
+        result['vis']['eval_vis'] = outs['eval_vis'] if 'eval_vis' in outs.keys() else None
+        if is_PSNR:
+            mse_ = torch.mean( (result['vis']['SR_UW'] - result['vis']['HR_UW']) ** 2 )
+            
+            PSNR = 10 * torch.log10(1 / mse_)
+            errs = collections.OrderedDict()
+            errs['PSNR'] = PSNR
+        else:
+            errs = None
+        result['errs'] = errs
+        return result
